fa34d6d27b517e566c351b32fd8fed54274e4bcd
Anderson <23005@aruplab.net>
2017-06-16 15:20:48 -0600

    Fixed a small coverage hole.

---
+++ src/test/core/squareFrom.spec.js
@@ -0,0 +1,9 @@
+import { squareFrom, active, empty, inactive } from '../../core/constants'
+
+describe("Square From Type", () => {
+    it("Returns the proper square", () => {
+        expect(squareFrom({ type: "active" })).toBe(active);
+        expect(squareFrom({ type: "inactive" })).toBe(inactive);
+        expect(squareFrom({ type: "empty" })).toBe(empty);
+    });
+});
