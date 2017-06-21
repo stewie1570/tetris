95d4f031351e314319282df0fe6ab6cde11c8050
Anderson <23005@aruplab.net>
2017-06-21 14:50:32 -0600

    Removes full rows.

--- src/domain/game.js
+++ src/domain/game.js
@@ -3,10 +3,10 @@ import { active, empty, inactive } from '../core/constants'
 import _ from 'lodash'
 
 export class Game {
-    iterate({ board, shapeProvider }) {
+    iterate({ board, shapeProvider, score }) {
         var activeIteration = () => {
             var interatedBoard = move({ board, to: { y: 1 } });
-            
+
             return {
                 board: board === interatedBoard
                     ? this._inactivedBoardFrom({ board })
@@ -16,10 +16,23 @@ export class Game {
 
         var newShapeIteration = () => {
             var newShape = _(shapeProvider()).flatMap((row, y) => row.map((value, x) => ({ x, y, value })));
+            var isFull = row => row.every(square => square === inactive);
+            var emptyRowFor = ({ y }) => _.range(0, board[y].length).map(() => empty);
+            var noFullRows = ({ board }) => {
+                var firstFullRowY = board.findIndex(isFull)
+                return firstFullRowY >= 0
+                    ? noFullRows({
+                        board: [emptyRowFor({ y: 0 })]
+                            .concat(board.slice(0, firstFullRowY))
+                            .concat(board.slice(firstFullRowY + 1, board.length))
+                    })
+                    : board;
+            };
+            var newBoard = noFullRows({ board });
 
             return {
-                board: board.map((row, y) => row.map((square, x) => _(newShape).some({ x, y, value: true }) ? active : square)),
-                isOver: board.some((row, y) => row.some((square, x) => square !== empty && _(newShape).some({ x, y, value: true })))
+                board: newBoard.map((row, y) => row.map((square, x) => _(newShape).some({ x, y, value: true }) ? active : square)),
+                isOver: newBoard.some((row, y) => row.some((square, x) => square !== empty && _(newShape).some({ x, y, value: true })))
             };
         }
 
--- src/test/domain/game.spec.js
+++ src/test/domain/game.spec.js
@@ -50,34 +50,52 @@ describe("Game", () => {
             ];
 
             expect(stringFrom(game.iterate({ board, shapeProvider }).board)).toEqual(stringFrom(tetrisBoard(`
+                **----
+                -*----
+                ------
+                --##--
+                ---#--`)));
+        });
+
+        describe("Full Row", () => {
+            it("moves rows above it down", () => {
+                var board = tetrisBoard(`
+                    ------
+                    ---###
+                    ######
+                    ######
+                    ---#--`);
+                var shapeProvider = () => [
+                    [true, true],
+                    [false, true]
+                ];
+
+                var result = game.iterate({ board, shapeProvider });
+
+                expect(stringFrom(result.board)).toEqual(stringFrom(tetrisBoard(`
                     **----
                     -*----
                     ------
-                    --##--
+                    ---###
                     ---#--`)));
+            });
+
+            it("moves rows above it down even when the last row is full", () => {
+                var board = tetrisBoard(`
+                    ------
+                    ---###
+                    ######`);
+                var shapeProvider = () => [
+                    [true, true],
+                    [false, true]
+                ];
+
+                expect(stringFrom(game.iterate({ board, shapeProvider }).board)).toEqual(stringFrom(tetrisBoard(`
+                    **----
+                    -*----
+                    ---###`)));
+            });
         });
-
-        // describe("Full Row", () => {
-        //     it("deletes the full row, moves rows above it down and increments the score", () => {
-        //         var board = tetrisBoard(`
-        //             ------
-        //             ---###
-        //             ######
-        //             ######
-        //             ---#--`);
-        //         var shapeProvider = () => [
-        //             [true, true],
-        //             [false, true]
-        //         ];
-
-        //         expect(stringFrom(game.iterate({ board, shapeProvider }).board)).toEqual(stringFrom(tetrisBoard(`
-        //             **----
-        //             -*----
-        //             ------
-        //             ---###
-        //             ---#--`)));
-        //     });
-        // });
 
         describe("Game Over", () => {
             it("is false when a new shape can be placed", () => {
