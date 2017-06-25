9016691a069620067c734b153a6cb63a43d52ef0
Stewart Anderson <stewie1570@gmail.com>
2017-06-25 00:16:36 -0600

    Added shapes, shape provider and the timer.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { TetrisBoard } from './components/board'
 import { tetrisBoard } from './domain/serialization'
 import { move, rotate } from './domain/motion'
+import { iterate } from './domain/game'
 import './App.css';
 
 var keys = {
@@ -11,49 +12,89 @@ var keys = {
     up: 38
 }
 
+var randomNumberGenerator = {
+    between: ({ min, max }) => Math.floor(Math.random() * max) + min
+}
+
+var shapes = [
+    [
+        [true, true],
+        [true, true]
+    ],
+    [
+        [true],
+        [true],
+        [true],
+        [true]
+    ],
+    [
+        [true, false],
+        [true, false]
+        [true, true]
+    ],
+    [
+        [false, true],
+        [false, true]
+        [true, true]
+    ]
+];
+
+var shapeProvider = () => shapes[randomNumberGenerator.between({ min: 0, max: shapes.length - 1 })];
+
 class App extends Component {
     constructor() {
         super();
 
         this.state = {
             board: tetrisBoard(`
-            -**-------
-            --*-------
-            --*-------
-            ----------
-            ----------
-            ----------
-            ----------
-            ----------
-            ----------
-            ----------
-            ----------
-            ----------
-            ----------
-            ----------
-            ----------
-            ----------
-            ----------
-            ----------
-            -----##---
-        `)
+                -**-------
+                --*-------
+                --*-------
+                ----------
+                ----------
+                ----------
+                ----------
+                ----------
+                ----------
+                ----------
+                ----------
+                ----------
+                ----------
+                ----------
+                ----------
+                ----------
+                ----------
+                ----------
+                ----------
+            `),
+            score: 0
         };
     }
 
     componentWillMount() {
         document.addEventListener("keydown", this.keyPress.bind(this), false);
+        this.resetTimer();
     }
 
     componentWillUnmount() {
         document.removeEventListener("keydown");
     }
 
+    resetTimer() {
+        this.timer && clearTimeout(this.timer);
+        this.timer = setTimeout(this.resetTimer.bind(this), 1000);
+
+        var { board, score } = this.state;
+
+        this.setState(iterate({ board, score, shapeProvider }));
+    }
+
     keyPress({ keyCode }) {
         var board = this.state.board;
         var newState = keyCode === keys.left ? { board: move({ board, to: { x: -1 } }) }
             : keyCode === keys.right ? { board: move({ board, to: { x: 1 } }) }
-            : keyCode === keys.down ? { board: move({ board, to: { y: 1 } }) }
-            : { board: rotate({ board }) };
+                : keyCode === keys.down ? { board: move({ board, to: { y: 1 } }) }
+                    : { board: rotate({ board }) };
 
         this.setState(newState);
     }
