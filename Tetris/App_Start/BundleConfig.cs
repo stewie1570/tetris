81249a568aa64aa69592a59811557e74b988e165
stewie1570 <stewie1570@hotmail.com>
2017-08-13 16:28:46 -0600

    Created a .NET (MVC / Web API) server for the app.

---
+++ Tetris/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Optimization;
+
+namespace Tetris
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new ScriptBundle("~/bundles/app")
+            //    .IncludeDirectory("~/Scripts", "*.js"));
+        }
+    }
+}
---
+++ Tetris/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tetris.Startup))]
+namespace Tetris
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
