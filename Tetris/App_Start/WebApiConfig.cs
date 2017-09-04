42482a86500e81baf2171f63ba0b5516016515b7
stewie1570 <stewie1570@hotmail.com>
2017-09-04 17:47:14 -0600

    Integrated Ninject and created the Users WebAPI controller.

--- Tetris/App_Start/WebApiConfig.cs
+++ Tetris/App_Start/WebApiConfig.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace Tetris
@@ -10,6 +8,8 @@ namespace Tetris
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            GlobalConfiguration.Configuration.Formatters.Clear();
+            GlobalConfiguration.Configuration.Formatters.Add(new JsonMediaTypeFormatter());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
---
+++ Tetris/Controllers/Api/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Tetris.Domain;
+
+namespace Tetris.Controllers.Api
+{
+    public class UsersController : ApiController
+    {
+        List<User> users;
+
+        public UsersController(Func<List<User>> getUsers)
+        {
+            this.users = getUsers();
+        }
+
+        [Route("api/users")]
+        [HttpGet]
+        public IEnumerable<dynamic> GetUsers()
+        {
+            return users.Select(user => new { user.Username, user.Score });
+        }
+    }
+}
