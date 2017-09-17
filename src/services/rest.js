1d4e112c16ceb677f2dbabca739136404a3a3874
stewie1570 <stewie1570@hotmail.com>
2017-09-16 23:29:58 -0600

    Client-side error handling is working.

--- src/services/rest.js
+++ src/services/rest.js
@@ -1,11 +1,14 @@
 import { ajax } from 'jquery'
 
+var errorHandled = request => request
+    .fail(msg => { throw new Error(msg && (msg.status == 0 ? 'Unable to communicate with server.' : msg.statusText) || msg); });
+
 export var Rest = {
-    get: url => ajax({ url, cache: false, type: "GET" }),
-    post: ({ url, data }) => ajax({
+    get: url => errorHandled(ajax({ url, cache: false, type: "GET" })),
+    post: ({ url, data }) => errorHandled(ajax({
         url,
         data: JSON.stringify(data),
         type: "POST",
         contentType: "application/json"
-    })
+    }))
 }
