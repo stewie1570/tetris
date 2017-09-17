bca202d9469dc39fdc7547c51d48153e744b8cfb
stewie1570 <stewie1570@hotmail.com>
2017-09-16 23:10:42 -0600

    Fixed a jQuery / rest API problem.

--- src/services/rest.js
+++ src/services/rest.js
@@ -2,5 +2,10 @@ import { ajax } from 'jquery'
 
 export var Rest = {
     get: url => ajax({ url, cache: false, type: "GET" }),
-    post: ({ url, data }) => ajax({ url, data, type: "POST" })
+    post: ({ url, data }) => ajax({
+        url,
+        data: JSON.stringify(data),
+        type: "POST",
+        contentType: "application/json"
+    })
 }
