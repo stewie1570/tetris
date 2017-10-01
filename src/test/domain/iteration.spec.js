e61fb721786b1c8cc6f25a764dbde3359e9148d7
Stewart Anderson <stewie1570@gmail.com>
2017-10-01 17:39:08 -0600

    Accidently removed the wrong spec file....oops.

---
+++ src/test/domain/iteration.spec.js
@@ -0,0 +1,150 @@
+import { tetrisBoard, stringFrom } from '../../domain/serialization'
+import { iterate, iterateUntilInactive } from '../../domain/iteration'
+
+
+describe("Iteration", () => {
+    it("iterate should move active squares down", () => {
+        var board = tetrisBoard(`
+            --**--
+            ------`);
+
+        expect(stringFrom(iterate({ board }).board)).toEqual(stringFrom(tetrisBoard(`
+            ------
+            --**--`)));
+    });
+
+    it("iterate should inactive board when active squares are at the bottom", () => {
+        var board = tetrisBoard(`
+            ------
+            --**--`);
+
+        expect(stringFrom(iterate({ board }).board)).toEqual(stringFrom(tetrisBoard(`
+            ------
+            --##--`)));
+    });
+
+    it("iterate should inactive board when active squares can't move down", () => {
+        var board = tetrisBoard(`
+            ------
+            --**--
+            ---#--`);
+
+        expect(stringFrom(iterate({ board }).board)).toEqual(stringFrom(tetrisBoard(`
+            ------
+            --##--
+            ---#--`)));
+    });
+
+    it("iterate should send a new shape when board is inactive", () => {
+        var board = tetrisBoard(`
+            ------
+            ------
+            ------
+            --##--
+            ---#--`);
+        var shapeProvider = () => [
+            [true, true],
+            [false, true]
+        ];
+
+        expect(stringFrom(iterate({ board, shapeProvider }).board)).toEqual(stringFrom(tetrisBoard(`
+            **----
+            -*----
+            ------
+            --##--
+            ---#--`)));
+    });
+
+    describe("Iterate Until Inactive", () => {
+        it("iterates the board until the active squares become inactive", () => {
+            var board = tetrisBoard(`
+                **----
+                -*----
+                ------
+                ---###
+                ---#--`);
+
+            expect(stringFrom(iterateUntilInactive({ board }))).toEqual(stringFrom(tetrisBoard(`
+                ------
+                ------
+                ------
+                ##-###
+                -#-#--`)));
+        });
+    });
+
+    describe("Full Row", () => {
+        it("moves rows above it down and increments the score", () => {
+            var board = tetrisBoard(`
+                ------
+                ---###
+                ######
+                ######
+                ---#--`);
+            var shapeProvider = () => [
+                [true, true],
+                [false, true]
+            ];
+
+            var result = iterate({ board, shapeProvider, score: 4 });
+
+            expect(stringFrom(result.board)).toEqual(stringFrom(tetrisBoard(`
+                **----
+                -*----
+                ------
+                ---###
+                ---#--`)));
+            expect(result.score).toBe(6);
+        });
+
+        it("moves rows above it down even when the last row is full", () => {
+            var board = tetrisBoard(`
+                ------
+                ---###
+                ######`);
+            var shapeProvider = () => [
+                [true, true],
+                [false, true]
+            ];
+
+            expect(stringFrom(iterate({ board, shapeProvider }).board)).toEqual(stringFrom(tetrisBoard(`
+                **----
+                -*----
+                ---###`)));
+        });
+    });
+
+    describe("Game Over", () => {
+        it("is false when a new shape can be placed", () => {
+            var board = tetrisBoard(`
+                ------
+                ------
+                ------
+                --##--
+                ---#--`);
+            var shapeProvider = () => [
+                [true, true],
+                [false, true]
+            ];
+
+            expect(iterate({ board, shapeProvider }).isOver).toBe(false);
+        });
+
+        it("is true when a new shape can't be placed", () => {
+            var board = tetrisBoard(`
+                ####--
+                ---#--`);
+            var shapeProvider = () => [
+                [true, true],
+                [false, true]
+            ];
+
+            var result = iterate({ board, shapeProvider });
+
+            expect(result.isOver).toBe(true);
+            expect(stringFrom(result.board)).toEqual(stringFrom(tetrisBoard(`
+                **##--
+                -*-#--`)));
+        });
+    });
+});
