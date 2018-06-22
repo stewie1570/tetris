d32a48f1dec0f5496e6356a31de3d1de92bba5ce
Stewart Anderson <stewie1570@gmail.com>
2018-06-21 18:10:17 -0600

    Changed the look of the error modal.

--- src/components/error-message.js
+++ src/components/error-message.js
@@ -24,17 +24,23 @@ export class ErrorMessage extends React.Component {
 
     render() {
         return <div
-            className="error-container"
-            ref={ref => this.errorContainer = ref}
-            style={{ display: this.state.visible ? 'block' : 'none' }}>
-            <div className="hide-container">
-                <span
-                    className="hide-text"
-                    onClick={this.hide}>
-                    X
-                </span>
+            className="modal"
+            style={{ display: this.state.visible ? "block" : "none" }}
+            role="dialog">
+            <div className="dialog-shade" />
+            <div className="modal-dialog" ref={ref => this.errorContainer = ref} role="document">
+                <div className="modal-content">
+                    <div className="modal-header modal-header-error">
+                        <b>Error</b>
+                        <button type="button" onClick={this.hide} className="close" aria-label="Close">
+                            <span>&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        {(this.state.errorMessage || "").replace("Uncaught Error: ", "")}
+                    </div>
+                </div>
             </div>
-            {(this.state.errorMessage || "").replace("Uncaught Error: ", "")}
         </div>;
     }
 }
