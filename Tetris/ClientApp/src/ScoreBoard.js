f39a234e12342e95b9f941edb26268530deaa06f
Stewart Anderson <stewie1570@gmail.com>
2021-08-08 18:24:22 -0600

    Adjusted running text for post-score-button.

--- Tetris/ClientApp/src/ScoreBoard.js
+++ Tetris/ClientApp/src/ScoreBoard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { CommandButton } from "./components/command-button";
 
-export function ScoreBoard({ game, postableScore, onPostScore: postScore, isLoading }) {
+export function ScoreBoard({ game, postableScore, onPostScore: postScore, isLoading, username }) {
   const allowScorePost = game.paused && Boolean(postableScore);
 
   const board = (game.scoreBoard || isLoading) &&
@@ -42,7 +42,7 @@ export function ScoreBoard({ game, postableScore, onPostScore: postScore, isLoad
         Would you like to post your score?
         <CommandButton
           className="btn btn-primary post-my-score-button"
-          runningText="Posting your score..."
+          runningText={Boolean(username) ? "Posting your score..." : undefined}
           onClick={postScore}>
           <span className="glyphicon glyphicon-send">&nbsp;</span>
           Post My Score ({postableScore})
--- Tetris/ClientApp/src/SinglePlayerGame.js
+++ Tetris/ClientApp/src/SinglePlayerGame.js
@@ -77,6 +77,7 @@ export const SinglePlayerGame = ({ shapeProvider }) => {
             />
             <ScoreBoard
               game={game}
+              username={username}
               isLoading={isLoadingScoreBoard}
               onPostScore={postScore}
               postableScore={postableScore} />
