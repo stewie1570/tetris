201a7df01cea590625940a08eaf8ad214fca57c0
Stewart Anderson <stewart.anderson3@extendhealth.com>
2021-09-16 16:40:47 -0600

    Added icons to mobile controls.

--- Tetris/ClientApp/src/components/MobileControls.js
+++ Tetris/ClientApp/src/components/MobileControls.js
@@ -5,21 +5,36 @@ import React from 'react'
 export function MobileControls(props) {
     return <div style={{ float: "left" }}>
         <button className="btn btn-primary left-control" onClick={() => props.onClick && props.onClick(keys.left)}>
-            <b>&lt;-</b>
+            <span className="glyphicon glyphicon-arrow-left">
+                &nbsp;
+            </span>
         </button>
         <button className="btn btn-primary right-control" onClick={() => props.onClick && props.onClick(keys.right)}>
-            <b>-&gt;</b>
+            <span className="glyphicon glyphicon-arrow-right">
+                &nbsp;
+            </span>
         </button>
         <button className="btn btn-primary top-control" onClick={() => props.onClick && props.onClick(keys.up)}>
+            <span className="glyphicon glyphicon-refresh">
+                &nbsp;
+            </span>
             <b>Rotate</b>
         </button>
         <button className="btn btn-primary top-stacked-control" onClick={props.onPause}>
+            <span className="glyphicon glyphicon-pause">
+                &nbsp;
+            </span>
             <b>Pause</b>
         </button>
         <button className="btn btn-primary bottom-stacked-control" onClick={() => props.onClick && props.onClick(keys.down)}>
-            <b>Down</b>
+            <span className="glyphicon glyphicon-arrow-down">
+                &nbsp;
+            </span>
         </button>
         <button className="btn btn-primary bottom-control" onClick={() => props.onClick && props.onClick(keys.space)}>
+            <span className="glyphicon glyphicon-check">
+                &nbsp;
+            </span>
             <b>Commit</b>
         </button>
     </div>;
