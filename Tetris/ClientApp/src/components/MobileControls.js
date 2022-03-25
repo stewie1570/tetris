a8e11d1aaff1a89001ca2f5540cbc2c5dc11f771
Stewart Anderson <stewart.anderson3@extendhealth.com>
2022-03-25 12:37:48 -0600

    MobileControls.css -> styled-components

--- Tetris/ClientApp/src/components/MobileControls.js
+++ Tetris/ClientApp/src/components/MobileControls.js
@@ -1,41 +1,95 @@
-import './MobileControls.css'
 import { keys } from '../core/constants'
 import React from 'react'
+import styled from 'styled-components';
+
+const LeftButton = styled.button`
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 15%;
+    height: 100%;
+    opacity: 0.5;
+`;
+
+const TopButton = styled.button`
+    position: fixed;
+    left: 15%;
+    top: 0px;
+    width: 70%;
+    height: 15%;
+    opacity: 0.5;
+`;
+
+const TopStackedButton = styled.button`
+    position: fixed;
+    left: 15%;
+    top: 15%;
+    width: 70%;
+    height: 15%;
+    opacity: 0.5;
+`;
+
+const RighButton = styled.button`
+    position: fixed;
+    top: 0px;
+    right: 0px;
+    width: 15%;
+    height: 100%;
+    opacity: 0.5;
+`;
+
+const BottomStackedButton = styled.button`
+    position: fixed;
+    left: 15%;
+    bottom: 15%;
+    width: 70%;
+    height: 15%;
+    opacity: 0.5;
+`;
+
+const BottomButton = styled.button`
+    position: fixed;
+    left: 15%;
+    bottom: 0px;
+    width: 70%;
+    height: 15%;
+    opacity: 0.5;
+`;
 
 export function MobileControls(props) {
     return <div style={{ position: "fixed", zIndex: 1 }}>
-        <button className="btn btn-primary left-control" onClick={() => props.onClick && props.onClick(keys.left)}>
+        <LeftButton className="btn btn-primary" onClick={() => props.onClick && props.onClick(keys.left)}>
             <span className="glyphicon glyphicon-arrow-left">
                 &nbsp;
             </span>
-        </button>
-        <button className="btn btn-primary right-control" onClick={() => props.onClick && props.onClick(keys.right)}>
+        </LeftButton>
+        <RighButton className="btn btn-primary" onClick={() => props.onClick && props.onClick(keys.right)}>
             <span className="glyphicon glyphicon-arrow-right">
                 &nbsp;
             </span>
-        </button>
-        <button className="btn btn-primary top-control" onClick={() => props.onClick && props.onClick(keys.up)}>
+        </RighButton>
+        <TopButton className="btn btn-primary" onClick={() => props.onClick && props.onClick(keys.up)}>
             <span className="glyphicon glyphicon-refresh">
                 &nbsp;
             </span>
             <b>Rotate</b>
-        </button>
-        {props.onPause && <button className="btn btn-primary top-stacked-control" onClick={props.onPause}>
+        </TopButton>
+        {props.onPause && <TopStackedButton className="btn btn-primary" onClick={props.onPause}>
             <span className="glyphicon glyphicon-pause">
                 &nbsp;
             </span>
             <b>Pause</b>
-        </button>}
-        <button className="btn btn-primary bottom-stacked-control" onClick={() => props.onClick && props.onClick(keys.down)}>
+        </TopStackedButton>}
+        <BottomStackedButton className="btn btn-primary" onClick={() => props.onClick && props.onClick(keys.down)}>
             <span className="glyphicon glyphicon-arrow-down">
                 &nbsp;
             </span>
-        </button>
-        <button className="btn btn-primary bottom-control" onClick={() => props.onClick && props.onClick(keys.space)}>
+        </BottomStackedButton>
+        <BottomButton className="btn btn-primary" onClick={() => props.onClick && props.onClick(keys.space)}>
             <span className="glyphicon glyphicon-check">
                 &nbsp;
             </span>
             <b>Commit</b>
-        </button>
+        </BottomButton>
     </div>;
 }
