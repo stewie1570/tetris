8aa7021c2c7cc1f60412470bfd889aeb29c8cefa
Stewart Anderson <stewart.anderson3@extendhealth.com>
2022-03-25 12:00:32 -0600

    Replaced Board.css w/ styled-components.

--- Tetris/ClientApp/src/components/TetrisBoard.js
+++ Tetris/ClientApp/src/components/TetrisBoard.js
@@ -1,16 +1,45 @@
 import React from "react";
 import { activeColumnRangeFrom } from "../domain/board";
-import "./Board.css";
-import { active, empty } from "../core/constants";
+import { empty } from "../core/constants";
+import styled from 'styled-components';
+
+const Square = styled.div`
+  width: 25px;
+  height: 25px;
+  margin: -0.5px;
+`;
+
+const Active = styled(Square)`
+  background-color: #00ff00;
+`;
+const Inactive = styled(Square)`
+  background-color: #22f;
+`;
+const ActiveEmpty = styled(Square)`
+  background-color: #111;
+`;
+const InactiveEmpty = styled(Square)`
+  background-color: #222;
+`;
+
+const Squares = {
+  'active': <Active data-testid="space" title="*" />,
+  'inactive': <Inactive data-testid="space" title="#" />,
+  'active-empty': <ActiveEmpty data-testid="space" title="-" />,
+  'inactive-empty': <InactiveEmpty data-testid="space" title="-" />
+}
 
 export function TetrisBoard({ board }) {
   const activeColumnRange = activeColumnRangeFrom({ board });
-  const squareClassNameFrom = ({ square, x }) =>
-    square === empty
+  const squareFrom = ({ square, x }) => {
+    const type = square === empty
       ? x >= activeColumnRange.x1 && x <= activeColumnRange.x2
         ? "active-empty"
         : "inactive-empty"
       : square.type;
+
+    return Squares[type];
+  }
 
   return (
     <table>
@@ -19,11 +48,7 @@ export function TetrisBoard({ board }) {
           <tr key={y} data-testid="row">
             {row.map((square, x) => (
               <td key={`${x},${y}`}>
-                <div
-                  className={`${squareClassNameFrom({ square, x })} square`}
-                  data-testid="space"
-                  title={square === empty ? "-" : square === active ? "*" : "#"}
-                ></div>
+                {squareFrom({ square, x })}
               </td>
             ))}
           </tr>
