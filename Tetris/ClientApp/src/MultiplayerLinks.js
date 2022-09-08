e3802d21b0dd1d090d9f5262640b0b065a1c0808
Stewart Anderson <stewart.anderson3@extendhealth.com>
2022-09-08 15:19:02 -0600

    pause the game when join multiplayer game is clicked

--- Tetris/ClientApp/src/MultiplayerLinks.js
+++ Tetris/ClientApp/src/MultiplayerLinks.js
@@ -1,29 +1,43 @@
 import React, { useContext } from "react";
 import { SinglePlayerGameContext } from "./SinglePlayerGame";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import { StringInput } from "./components/Prompt";
 
 export const MultiplayerLinks = () => {
-    const { prompt } = useContext(SinglePlayerGameContext);
-    const navigate = useNavigate();
+  const { prompt, game } = useContext(SinglePlayerGameContext);
+  const navigate = useNavigate();
 
-    const goToMultiplayerGame = () => prompt(exitModal => <StringInput
-        filter={value => (value ?? "").trim()}
-        inputFilter={value => value.toUpperCase().substring(0, 5)}
-        onSubmitString={organizerId => {
-            organizerId && navigate(`/${organizerId}`);
-            exitModal();
-        }}>
+  const goToMultiplayerGame = async () => {
+    if (!game.paused) {
+        game?.behaviors?.pause?.();
+    }
+    await prompt((exitModal) => (
+      <StringInput
+        filter={(value) => (value ?? "").trim()}
+        inputFilter={(value) => value.toUpperCase().substring(0, 5)}
+        onSubmitString={(organizerId) => {
+          organizerId && navigate(`/${organizerId}`);
+          exitModal();
+        }}
+      >
         Code:
-    </StringInput>);
+      </StringInput>
+    ));
+  };
 
-    return <>
-        <Link
-            style={{ marginTop: "1rem", display: "block" }}
-            to={`/host`}>Host Multiplayer Game</Link>
+  return (
+    <>
+      <Link style={{ marginTop: "1rem", display: "block" }} to={`/host`}>
+        Host Multiplayer Game
+      </Link>
 
-        <button type="button" className="btn btn-link" onClick={goToMultiplayerGame}>
-            Join Multiplayer Game
-        </button>
-    </>;
+      <button
+        type="button"
+        className="btn btn-link"
+        onClick={goToMultiplayerGame}
+      >
+        Join Multiplayer Game
+      </button>
+    </>
+  );
 };
