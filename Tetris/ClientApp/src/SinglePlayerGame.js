60a636fa621bceebf948f9319055d5305d03dd09
Stewart Anderson <stewart.anderson3@extendhealth.com>
2022-09-09 10:49:00 -0600

    pause when starting prompting for multiplayer code

--- Tetris/ClientApp/src/SinglePlayerGame.js
+++ Tetris/ClientApp/src/SinglePlayerGame.js
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
 import { TetrisGame, emptyBoard } from "./components/TetrisGame";
-import { StringInput as StringPrompt, usePrompt } from './components/Prompt';
+import { StringInput as StringPrompt, usePrompt } from "./components/Prompt";
 import { leaderBoardService } from "./services";
 import "./App.css";
 import { ScoreBoard } from "./ScoreBoard";
 import { GameControls } from "./GameControls";
-import { useLoadingState, useMountedOnlyState } from 'leaf-validator';
+import { useLoadingState, useMountedOnlyState } from "leaf-validator";
 import { GameMetaFrame } from "./components/GameMetaFrame";
 
 export const SinglePlayerGameContext = React.createContext();
@@ -16,7 +16,7 @@ export const initialGameState = {
   mobile: false,
   oldScore: 0,
   paused: false,
-  score: 0
+  score: 0,
 };
 
 export const SinglePlayerGameContextProvider = ({ children }) => {
@@ -24,26 +24,6 @@ export const SinglePlayerGameContextProvider = ({ children }) => {
   const [username, setUsername] = useMountedOnlyState();
   const { dialogProps, prompt } = usePrompt();
 
-  return <SinglePlayerGameContext.Provider value={{
-    game,
-    setGame,
-    username,
-    setUsername,
-    dialogProps,
-    prompt
-  }}>
-    {children}
-  </SinglePlayerGameContext.Provider>;
-};
-
-export const SinglePlayerGame = ({ shapeProvider, children: otherPlayers, header, additionalControls, ...otherProps }) => {
-  const {
-    game,
-    setGame,
-    username,
-    setUsername,
-    prompt
-  } = useContext(SinglePlayerGameContext);
   const [isLoadingScoreBoard, showLoadingScoreBoardWhile] = useLoadingState();
 
   const postableScore = game.score || game.oldScore;
@@ -52,77 +32,157 @@ export const SinglePlayerGame = ({ shapeProvider, children: otherPlayers, header
   const postScore = async () => {
     const hasUserName = Boolean(username?.trim().length);
 
-    const sendCurrentScoreFor = name => leaderBoardService
-      .postScore({
-        username: name,
-        score: postableScore
-      })
-      .then(reloadScoreBoard)
-      .then(scoreBoard => {
-        const scoreIsOnBoard = scoreBoard.find(({ username }) => username === name);
-        !scoreIsOnBoard && window.dispatchEvent(new CustomEvent("user-error", {
-          detail: `Your score was recorded but didn't make the top ${scoreBoard.length}.`
-        }));
-      })
+    const sendCurrentScoreFor = (name) =>
+      leaderBoardService
+        .postScore({
+          username: name,
+          score: postableScore,
+        })
+        .then(reloadScoreBoard)
+        .then((scoreBoard) => {
+          const scoreIsOnBoard = scoreBoard.find(
+            ({ username }) => username === name
+          );
+          !scoreIsOnBoard &&
+            window.dispatchEvent(
+              new CustomEvent("user-error", {
+                detail: `Your score was recorded but didn't make the top ${scoreBoard.length}.`,
+              })
+            );
+        });
 
-    const promptUserNameAndSendScore = () => prompt(exitModal => <StringPrompt
-      filter={value => (value ?? "").trim()}
-      onSubmitString={name => Boolean(name?.length)
-        ? sendCurrentScoreFor(name)
-          .then(() => setUsername(name))
-          .then(exitModal, exitModal)
-        : exitModal()}
-      submittingText="Posting Your Score...">
-      What user name would you like?
-    </StringPrompt>);
+    const promptUserNameAndSendScore = () =>
+      prompt((exitModal) => (
+        <StringPrompt
+          filter={(value) => (value ?? "").trim()}
+          onSubmitString={(name) =>
+            Boolean(name?.length)
+              ? sendCurrentScoreFor(name)
+                  .then(() => setUsername(name))
+                  .then(exitModal, exitModal)
+              : exitModal()
+          }
+          submittingText="Posting Your Score..."
+        >
+          What user name would you like?
+        </StringPrompt>
+      ));
 
-    await (hasUserName ? sendCurrentScoreFor(username) : promptUserNameAndSendScore());
-  }
+    await (hasUserName
+      ? sendCurrentScoreFor(username)
+      : promptUserNameAndSendScore());
+  };
 
   const reloadScoreBoard = async () => {
     const scoreBoard = await leaderBoardService.get();
-    setGame(game => ({ ...game, scoreBoard }));
+    setGame((game) => ({ ...game, scoreBoard }));
     return scoreBoard;
   };
 
-  const pause = async () => {
+  const pause = async ({ showScoreBoard }) => {
     const paused = !game.paused;
-    setGame({ ...game, paused, scoreBoard: paused ? game.scoreBoard : undefined });
-    paused && !otherPlayers && await showLoadingScoreBoardWhile(reloadScoreBoard());
-  }
+    setGame({
+      ...game,
+      paused,
+      scoreBoard: paused ? game.scoreBoard : undefined,
+    });
+    paused &&
+      showScoreBoard &&
+      (await showLoadingScoreBoardWhile(reloadScoreBoard()));
+  };
+
+  return (
+    <SinglePlayerGameContext.Provider
+      value={{
+        game,
+        setGame,
+        username,
+        setUsername,
+        dialogProps,
+        prompt,
+        isLoadingScoreBoard,
+        showLoadingScoreBoardWhile,
+        postableScore,
+        allowScorePost,
+        postScore,
+        reloadScoreBoard,
+        pause,
+      }}
+    >
+      {children}
+    </SinglePlayerGameContext.Provider>
+  );
+};
+
+export const SinglePlayerGame = ({
+  shapeProvider,
+  children: otherPlayers,
+  header,
+  additionalControls,
+  ...otherProps
+}) => {
+  const {
+    game,
+    setGame,
+    username,
+    pause,
+    allowScorePost,
+    isLoadingScoreBoard,
+    postableScore,
+    postScore,
+  } = useContext(SinglePlayerGameContext);
 
   return (
     <GameMetaFrame
       {...otherProps}
-      header={<>
-        {header}
-        <p>
-          {`Score: ${game.score}` +
-            (game.oldScore
-              ? ` (Previous: ${game.oldScore})`
-              : "")}
-        </p>
-      </>}
-      game={<TetrisGame
-        game={game}
-        onChange={setGame}
-        shapeProvider={shapeProvider}
-        onPause={!otherPlayers && pause}
-      />}
-      scoreBoard={game.paused && (otherPlayers || <ScoreBoard
-        allowScorePost={allowScorePost}
-        game={game}
-        username={username}
-        isLoading={isLoadingScoreBoard}
-        onPostScore={postScore}
-        postableScore={postableScore} />)}
-      controls={<><GameControls
-        game={game}
-        onPause={!otherPlayers && pause}
-        onToggleMobile={() => setGame(game => ({ ...game, mobile: !game.mobile }))} />
-        {additionalControls}
-      </>} />
+      header={
+        <>
+          {header}
+          <p>
+            {`Score: ${game.score}` +
+              (game.oldScore ? ` (Previous: ${game.oldScore})` : "")}
+          </p>
+        </>
+      }
+      game={
+        <TetrisGame
+          game={game}
+          onChange={setGame}
+          shapeProvider={shapeProvider}
+          onPause={
+            !otherPlayers && (() => pause({ showScoreBoard: !otherPlayers }))
+          }
+        />
+      }
+      scoreBoard={
+        game.paused &&
+        (otherPlayers || (
+          <ScoreBoard
+            allowScorePost={allowScorePost}
+            game={game}
+            username={username}
+            isLoading={isLoadingScoreBoard}
+            onPostScore={postScore}
+            postableScore={postableScore}
+          />
+        ))
+      }
+      controls={
+        <>
+          <GameControls
+            game={game}
+            onPause={
+              !otherPlayers && (() => pause({ showScoreBoard: !otherPlayers }))
+            }
+            onToggleMobile={() =>
+              setGame((game) => ({ ...game, mobile: !game.mobile }))
+            }
+          />
+          {additionalControls}
+        </>
+      }
+    />
   );
-}
+};
 
 export default SinglePlayerGame;
