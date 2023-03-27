71049d9baa22940ade430178a01c6833c513ac5d
Stewart Anderson <stewart.anderson3@extendhealth.com>
2023-03-27 12:25:13 -0600

    4 char IDs

--- Tetris/ClientApp/src/randomIdGenerator.js
+++ Tetris/ClientApp/src/randomIdGenerator.js
@@ -1,1 +1,1 @@
-export const randomIdGenerator = () => Math.random().toString(36).slice(-5).toUpperCase();
+export const randomIdGenerator = () => Math.random().toString(36).slice(-4).toUpperCase();
