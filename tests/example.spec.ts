485f01b88371c835a99ba1214758238c5a6addd3
Stewart Anderson <stewart.anderson3@extendhealth.com>
2023-06-12 17:13:20 -0600

    adding playwright

---
+++ tests/example.spec.ts
@@ -0,0 +1,47 @@
+import { test, chromium } from '@playwright/test';
+
+test('start a multiplayer game', async () => {
+  test.setTimeout(60000);
+  const { page: browserPage1, context: context1 } = await newBrowserPage();
+  const { page: browserPage2, context: context2 } = await newBrowserPage();
+
+  await browserPage1.goto('https://localhost:5001/');
+  await browserPage1.getByRole('link', { name: 'Host Multiplayer Game' }).click();
+  const gameRoomCode = await browserPage1
+    .getByRole('cell', { name: 'Code', exact: true })
+    .locator('..')
+    .getByRole('cell')
+    .nth(1)
+    .textContent();
+  await browserPage1.getByRole('button', { name: 'Set User Name' }).click();
+  const userNamePromptLabel = browserPage1.getByLabel('What user name would you like?');
+  await userNamePromptLabel.fill('browser page 1');
+  await userNamePromptLabel.press('Enter');
+  await browserPage1.getByRole('button', { name: 'Start Game' });
+
+  await browserPage2.goto('https://localhost:5001/');
+  await browserPage2.getByRole('button', { name: 'Join Multiplayer Game' }).click();
+  await browserPage2.getByRole('dialog')
+    .filter({ hasText: 'Error×An error occurred.' })
+    .getByRole('button', { name: 'Close' })
+    .click();
+  await browserPage2.getByLabel('Code:').click();
+  await browserPage2.getByLabel('Code:').fill(gameRoomCode ?? '');
+  await browserPage2.getByRole('button', { name: ' Ok' }).click();
+  await browserPage2.getByRole('button', { name: 'Set User Name' }).click();
+  await browserPage2.getByLabel('What user name would you like?').fill('browser page 2');
+  await browserPage2.getByLabel('What user name would you like?').press('Enter');
+  await browserPage2.getByRole('button', { name: 'Start Game' }).click();
+
+  await context1.close();
+  await context2.close();
+});
+
+async function newBrowserPage() {
+  // const browser = await chromium.launch({ slowMo: 2000 });
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  return { browser, context, page };
+}
+
