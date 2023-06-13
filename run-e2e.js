c4d06bbea32f6c815a45c440eada9dee582972d7
Stewart Anderson <stewart.anderson3@extendhealth.com>
2023-06-13 14:10:39 -0600

    try another failure path

--- run-e2e.js
+++ run-e2e.js
@@ -2,8 +2,6 @@ const { execSync } = require("child_process");
 
 try {
   execSync("playwright test", { stdio: "inherit" });
-  execSync("npm run stop-server", { stdio: "inherit" });
-} catch (error) {
-  execSync("npm run stop-server", { stdio: "inherit" });
-  throw error;
+} finally {
+  execSync("pkill dotnet", { stdio: "inherit" });
 }
