6003e54cecf8871fa6d7605d21c97c0c8fcf4322
Stewart Anderson <stewart.anderson3@extendhealth.com>
2023-06-14 13:02:28 -0600

    running e2e on Windows And Linux

--- run-e2e.js
+++ run-e2e.js
@@ -3,5 +3,10 @@ const { execSync } = require("child_process");
 try {
   execSync("playwright test", { stdio: "inherit" });
 } finally {
-  execSync("pkill dotnet", { stdio: "inherit" });
+  const linuxKillCommand = "pkill dotnet";
+  const windowsKillCommand = "taskkill /IM dotnet.exe /F";
+  const isWindows = process.platform === "win32";
+  execSync(isWindows ? windowsKillCommand : linuxKillCommand, {
+    stdio: "inherit",
+  });
 }
