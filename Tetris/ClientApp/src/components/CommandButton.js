2576d0dd7ab29eb010f305652748113b767fb822
Stewart Anderson <stewart.anderson3@extendhealth.com>
2023-11-20 19:38:17 -0700

    buttons should not lose focus during loading state

--- Tetris/ClientApp/src/components/CommandButton.js
+++ Tetris/ClientApp/src/components/CommandButton.js
@@ -1,20 +1,24 @@
-import React, { forwardRef } from 'react';
-import { useLoadingState } from 'leaf-validator'
+import React, { forwardRef } from "react";
+import { useLoadingState } from "leaf-validator";
 
-export const CommandButton = forwardRef(({ onClick, runningText, disabled, type, children, ...otherProps }, ref) => {
+export const CommandButton = forwardRef(
+  ({ onClick, runningText, type, children, ...otherProps }, ref) => {
     const [isRunning, showRunningWhile] = useLoadingState();
 
-    const click = async source => {
-        await onClick && !isRunning && showRunningWhile(onClick(source))
-    }
+    const click = async (source) => {
+      (await onClick) && !isRunning && showRunningWhile(onClick(source));
+    };
 
-    return <button
+    return (
+      <button
         style={{ width: "90%" }}
         {...otherProps}
         ref={ref}
         onClick={click}
-        disabled={disabled || isRunning}
-        type={type || "button"}>
+        type={type || "button"}
+      >
         {isRunning && runningText ? runningText : children}
-    </button>;
-});
+      </button>
+    );
+  }
+);
