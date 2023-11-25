beaa7c95abbfc6ff5023759cf6625d33179631df
Stewart Anderson <stewie1570@gmail.com>
2023-11-24 19:17:37 -0700

    improved the isMobile check

--- Tetris/ClientApp/src/hooks/useIsMobile.js
+++ Tetris/ClientApp/src/hooks/useIsMobile.js
@@ -2,15 +2,24 @@ import { useState } from 'react';
 import { useLifeCycle } from './useLifeCycle';
 
 const threshold = 768;
+const mobileUserAgentPattern = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+
+function isMobileCheck() {
+    const userAgent = navigator.userAgent;
+    const isMobileUserAgent = mobileUserAgentPattern.test(userAgent);
+    const isMobileResolution = window.innerWidth < threshold;
+
+    return isMobileResolution || isMobileUserAgent;
+}
 
 export function useIsMobile() {
-    const [isMobile, setIsMobile] = useState(window.innerWidth < threshold);
-    const handleResize = () => setIsMobile(window.innerWidth < threshold);
+    const [isMobile, setIsMobile] = useState(isMobileCheck());
+    const handleResize = () => setIsMobile(isMobileCheck());
 
     useLifeCycle({
         onMount: () => window.addEventListener('resize', handleResize),
         onUnMount: () => window.removeEventListener('resize', handleResize)
-    })
+    });
 
     return isMobile;
 }
