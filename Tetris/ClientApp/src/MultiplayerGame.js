42231d7250ef3a16c0e15a32efe3bef47cb2c396
Stewart Anderson <stewie1570@gmail.com>
2025-01-07 22:20:05 -0700

    results page to not include players that didn't play

--- Tetris/ClientApp/src/MultiplayerGame.js
+++ Tetris/ClientApp/src/MultiplayerGame.js
@@ -97,22 +97,22 @@ export const MultiplayerGame = ({ shapeProvider }) => {
         onSubmitString={async (name) => {
           name
             ? await gameHub.invoke
-                .status({
-                  groupId: organizerUserId,
-                  message: {
-                    userId: currentUserId,
-                    name: name,
-                  },
-                })
-                .then(() => setUsername(name))
-                .then(exitModal)
-                .catch(({ message }) =>
-                  window.dispatchEvent(
-                    new CustomEvent("user-error", {
-                      detail: trimHubExceptionMessage(message),
-                    })
-                  )
+              .status({
+                groupId: organizerUserId,
+                message: {
+                  userId: currentUserId,
+                  name: name,
+                },
+              })
+              .then(() => setUsername(name))
+              .then(exitModal)
+              .catch(({ message }) =>
+                window.dispatchEvent(
+                  new CustomEvent("user-error", {
+                    detail: trimHubExceptionMessage(message),
+                  })
                 )
+              )
             : exitModal();
         }}
         submittingText={
@@ -209,41 +209,44 @@ export const MultiplayerGame = ({ shapeProvider }) => {
 
   const results = gameResults
     ? () => (
-        <Centered>
-          <Header style={{ width: "90%", display: "inline-block" }}>
-            Game Over
-          </Header>
-          <div
-            className="card mb-3"
-            style={{ display: "inline-block", width: "90%", textAlign: "left" }}
-          >
-            <div className="card-header">Results</div>
-            <div className="card-body p-0">
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Score</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {Object.keys(otherPlayers).map((userId) => (
+      <Centered>
+        <Header style={{ width: "90%", display: "inline-block" }}>
+          Game Over
+        </Header>
+        <div
+          className="card mb-3"
+          style={{ display: "inline-block", width: "90%", textAlign: "left" }}
+        >
+          <div className="card-header">Results</div>
+          <div className="card-body p-0">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Score</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object
+                  .keys(otherPlayers)
+                  .filter(userId => !otherPlayers[userId].disconnected || otherPlayers[userId].score)
+                  .map((userId) => (
                     <tr key={userId}>
                       <td>
                         {otherPlayers[userId].name ?? "[Un-named Player]"}
                       </td>
-                      <td>{gameResults[userId]?.score}</td>
+                      <td>{gameResults[userId].score}</td>
                     </tr>
                   ))}
-                </tbody>
-              </table>
-            </div>
+              </tbody>
+            </table>
           </div>
-          <GameChat style={{ width: "90%", display: "inline-block" }} />
-          <div>{initiallyDisabledPlayerGameLink}</div>
-          <div>{resetButton}</div>
-        </Centered>
-      )
+        </div>
+        <GameChat style={{ width: "90%", display: "inline-block" }} />
+        <div>{initiallyDisabledPlayerGameLink}</div>
+        <div>{resetButton}</div>
+      </Centered>
+    )
     : undefined;
 
   const retryButton = (
@@ -274,41 +277,41 @@ export const MultiplayerGame = ({ shapeProvider }) => {
   const waitingForOrganizer =
     !organizerConnectionStatus && !isOrganizer
       ? () => (
-          <CenterScreen>
-            <Header>Waiting for organizer...</Header>
-            <Centered>
-              <div>{singlePlayerGameLink}</div>
-              <div>{retryButton}</div>
-            </Centered>
-          </CenterScreen>
-        )
+        <CenterScreen>
+          <Header>Waiting for organizer...</Header>
+          <Centered>
+            <div>{singlePlayerGameLink}</div>
+            <div>{retryButton}</div>
+          </Centered>
+        </CenterScreen>
+      )
       : undefined;
 
   const organizerDisconnected =
     organizerConnectionStatus === "disconnected" && !isOrganizer && game.paused
       ? () => (
-          <CenterScreen>
-            <Header>Organizer has disconnected.</Header>
-            <Centered>
-              <div>{singlePlayerGameLink}</div>
-              <div>{retryButton}</div>
-            </Centered>
-          </CenterScreen>
-        )
+        <CenterScreen>
+          <Header>Organizer has disconnected.</Header>
+          <Centered>
+            <div>{singlePlayerGameLink}</div>
+            <div>{retryButton}</div>
+          </Centered>
+        </CenterScreen>
+      )
       : undefined;
 
   const userIsDisconnected =
     isConnected === undefined
       ? () => (
-          <CenterScreen>
-            <Header>
-              <Spinner /> Connecting to game server...
-            </Header>
-            <Centered>
-              <div>{singlePlayerGameLink}</div>
-            </Centered>
-          </CenterScreen>
-        )
+        <CenterScreen>
+          <Header>
+            <Spinner /> Connecting to game server...
+          </Header>
+          <Centered>
+            <div>{singlePlayerGameLink}</div>
+          </Centered>
+        </CenterScreen>
+      )
       : undefined;
 
   const gameHeader = (
